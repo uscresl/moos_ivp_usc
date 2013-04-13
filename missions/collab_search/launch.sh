9fc88122619d0b8fb341f5d4aae1f9cba86faf28
Stephanie Kemna <kemna@usc.edu>
2013-04-13 15:55:00 -0700

    adding collision avoidance, opregion bhv, second vehicle

--- missions/collab_search/launch.sh
+++ missions/collab_search/launch.sh
@@ -34,8 +34,11 @@ fi
 #-------------------------------------------------------
 #  Part 2: Create the .moos and .bhv files. 
 #-------------------------------------------------------
-VNAME1="archie"      # The first  vehicle community
-START_POS1="0,0"  
+VNAME1="ben"       # The first  vehicle community
+START_POS1="0,0"
+
+VNAME2="tim"      # The second vehicle community
+START_POS2="0,0"
 
 nsplug meta_shoreside.moos targ_shoreside.moos -f WARP=$TIME_WARP \
    VNAME="shoreside" HAZARD_FILE=$HAZARD_FILE   
@@ -43,10 +46,18 @@ nsplug meta_shoreside.moos targ_shoreside.moos -f WARP=$TIME_WARP \
 nsplug meta_vehicle.moos targ_$VNAME1.moos -f WARP=$TIME_WARP  \
    VNAME=$VNAME1      START_POS=$START_POS1                    \
    VPORT="9001"       SHARE_LISTEN="9301"                      \
-   VTYPE=UUV          
+   VTYPE=UUV
+
+nsplug meta_vehicle.moos targ_$VNAME2.moos -f WARP=$TIME_WARP  \
+   VNAME=$VNAME2      START_POS=$START_POS2                    \
+   VPORT="9002"       SHARE_LISTEN="9302"                      \
+   VTYPE=UUV
 
 nsplug meta_vehicle.bhv targ_$VNAME1.bhv -f VNAME=$VNAME1      \
     START_POS=$START_POS1 
+
+nsplug meta_vehicle.bhv targ_$VNAME2.bhv -f VNAME=$VNAME2      \
+    START_POS=$START_POS2 
 
 if [ ${JUST_MAKE} = "yes" ] ; then
     exit 0
@@ -58,6 +69,11 @@ fi
 printf "Launching $VNAME1 MOOS Community (WARP=%s) \n" $TIME_WARP
 pAntler targ_$VNAME1.moos >& /dev/null &
 sleep .25
+
+printf "Launching $VNAME2 MOOS Community (WARP=%s) \n" $TIME_WARP
+pAntler targ_$VNAME2.moos >& /dev/null &
+sleep .25
+
 printf "Launching $SNAME MOOS Community (WARP=%s) \n"  $TIME_WARP
 pAntler targ_shoreside.moos >& /dev/null &
 printf "Done \n"
@@ -65,7 +81,7 @@ printf "Done \n"
 uMAC targ_shoreside.moos
 
 printf "Killing all processes ... \n"
-kill %1 %2 
+kill %1 %2 %3
 printf "Done killing processes.   \n"
 
 
