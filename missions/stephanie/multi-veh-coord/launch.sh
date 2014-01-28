d61de75f4702af4bc6c72dbe0bedbf04def51bdb
Stephanie Kemna <kemna@usc.edu>
2014-01-27 17:12:37 -0800

    adding folder to start a multi-veh coord problem/simulation

---
+++ missions/stephanie/multi-veh-coord/launch.sh
@@ -0,0 +1,95 @@
+#!/bin/bash 
+#-------------------------------------------------------
+#  Part 1: Check for and handle command-line arguments
+#-------------------------------------------------------
+TIME_WARP=1
+JUST_MAKE="no"
+
+for ARGI; do
+    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
+        printf "%s [SWITCHES] [time_warp]   \n" $0
+        printf "  --just_make, -j    \n" 
+        printf "  --help, -h         \n" 
+        exit 0;
+    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
+        TIME_WARP=$ARGI
+    elif [ "${ARGI}" = "--just_build" -o "${ARGI}" = "-j" ] ; then
+        JUST_MAKE="yes"
+    else 
+        printf "Bad Argument: %s \n" $ARGI
+        exit 0
+    fi
+done
+
+#-------------------------------------------------------
+#  Part 2: Create the .moos and .bhv files. 
+#-------------------------------------------------------
+VNAME1="anton"        # The first  vehicle community
+START_POS1="100,30"
+WAYPOINTS1="100,0:200,0:200,-100:100,-100"
+
+VNAME2="bernard"      # The second vehicle community
+START_POS2="200,70"
+WAYPOINTS2="200,0:300,0:300,-100:200,-100"
+
+VNAME3="cornelis"     # The third vehicle community
+START_POS3="0,0"
+WAYPOINTS3="100,-100:200,-100:200,-200:100,-200"
+
+nsplug meta_shoreside.moos targ_shoreside.moos -f WARP=$TIME_WARP \
+   VNAME="shoreside"
+
+nsplug meta_vehicle.moos targ_$VNAME1.moos -f WARP=$TIME_WARP  \
+   VNAME=$VNAME1      START_POS=$START_POS1                    \
+   VPORT="9001"       SHARE_LISTEN="9301"                      \
+   VTYPE=UUV
+
+nsplug meta_vehicle.moos targ_$VNAME2.moos -f WARP=$TIME_WARP  \
+   VNAME=$VNAME2      START_POS=$START_POS2                    \
+   VPORT="9002"       SHARE_LISTEN="9302"                      \
+   VTYPE=UUV
+
+nsplug meta_vehicle.moos targ_$VNAME3.moos -f WARP=$TIME_WARP  \
+   VNAME=$VNAME3      START_POS=$START_POS3                    \
+   VPORT="9003"       SHARE_LISTEN="9303"                      \
+   VTYPE=UUV
+
+nsplug meta_vehicle.bhv targ_$VNAME1.bhv -f VNAME=$VNAME1      \
+    START_POS=$START_POS1 WAYPOINTS=$WAYPOINTS1
+
+nsplug meta_vehicle.bhv targ_$VNAME2.bhv -f VNAME=$VNAME2      \
+    START_POS=$START_POS2 WAYPOINTS=$WAYPOINTS2
+
+nsplug meta_vehicle.bhv targ_$VNAME3.bhv -f VNAME=$VNAME3      \
+    START_POS=$START_POS3 WAYPOINTS=$WAYPOINTS3
+
+if [ ${JUST_MAKE} = "yes" ] ; then
+    exit 0
+fi
+
+#-------------------------------------------------------
+#  Part 3: Launch the processes
+#-------------------------------------------------------
+printf "Launching $VNAME1 MOOS Community (WARP=%s) \n" $TIME_WARP
+pAntler targ_$VNAME1.moos >& /dev/null &
+sleep .25
+
+printf "Launching $VNAME2 MOOS Community (WARP=%s) \n" $TIME_WARP
+pAntler targ_$VNAME2.moos >& /dev/null &
+sleep .25
+
+printf "Launching $VNAME3 MOOS Community (WARP=%s) \n" $TIME_WARP
+pAntler targ_$VNAME3.moos >& /dev/null &
+sleep .25
+
+printf "Launching $SNAME MOOS Community (WARP=%s) \n"  $TIME_WARP
+pAntler targ_shoreside.moos >& /dev/null &
+printf "Done \n"
+
+uMAC targ_shoreside.moos
+
+printf "Killing all processes ... \n"
+kill %1 %2 %3 %4
+printf "Done killing processes.   \n"
+
+
