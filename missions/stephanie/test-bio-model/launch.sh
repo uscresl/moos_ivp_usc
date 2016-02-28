946004555fd25255ac92f356852f03e8f0fab736
Stephanie Kemna <kemna@usc.edu>
2016-02-27 17:25:39 -0800

    set up sim for both lawnmower and adaptive

--- missions/stephanie/test-bio-model/launch.sh
+++ missions/stephanie/test-bio-model/launch.sh
@@ -4,6 +4,7 @@
 #-------------------------------------------------------
 TIME_WARP=1
 JUST_MAKE="no"
+ADAPTIVE="no"
 
 for ARGI; do
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
@@ -15,6 +16,8 @@ for ARGI; do
         TIME_WARP=$ARGI
     elif [ "${ARGI}" = "--just_build" -o "${ARGI}" = "-j" ] ; then
         JUST_MAKE="yes"
+    elif [ "${ARGI}" = "--adaptive" -o "${ARGI}" = "-a" ] ; then
+        ADAPTIVE="yes"
     else 
         printf "Bad Argument: %s \n" $ARGI
         exit 0
@@ -31,18 +34,42 @@ MSGDIR="../../../modem-msgs"
 
 PAINTSEGLIST="pts={500,1200:500,1000:900,1000:900,1200:500,1200},label=survey_area,label_color=white,edge_color=green,vertex_color=green,vertex_size=2,edge_size=2"
 
-# config for static lawnmower, copied from test-lawnmowers
-LAWNMOWER_CONFIG="format=lawnmower,label=east-west-survey,x=700,y=1100,width=400,height=200,lane_width=20,degs=0,startx=0,starty=0"
-
 # config for lawnmower used to get pilot data for autotuning hyperparams GP
 PILOT_LAWNMOWER_CONFIG="format=lawnmower,label=pilot-survey,x=700,y=1100,width=400,height=200,lane_width=100,degs=0,startx=0,starty=0"
 PILOT_LAWNMOWER_C_NS="$PILOT_LAWNMOWER_CONFIG,rows=north-south"
 PILOT_LAWNMOWER_C_EW="$PILOT_LAWNMOWER_CONFIG,rows=east-west"
 
+# loiter for during hyperparameter optimization
+HP_LOITER_CONFIG="format=radial,x=440,y=970,radius=10,pts=4,snap=1,label=hp_optimization_loiter"
+
+if [ "${ADAPTIVE}" = "no" ] ; then
+  # lawnmower
+
+  # config for lawnmower for actual GP model building
+  LAWNMOWER="format=lawnmower,label=east-west-survey,x=700,y=1100,width=400,height=200,lane_width=20,degs=0,startx=0,starty=0"
+  LAWNMOWEREW="$LAWNMOWER,rows=east-west"
+  LAWNMOWERNS="$LAWNMOWER,rows=north-south"  
+  # ports
+  ANNA_LISTEN="8301"
+  ANNA_VPORT="8001"
+  SHORE_LISTEN="8300"
+  SHORE_VPORT="8000"
+else
+  # adaptive
+  
+  # ports
+  ANNA_LISTEN="9301"
+  ANNA_VPORT="9001"
+  SHORE_LISTEN="9300"
+  SHORE_VPORT="9000"
+fi
+
+
+
 SERVERHOST="localhost" #"localhost"
 nsplug meta_shoreside.moos targ_shoreside.moos -f WARP=$TIME_WARP \
    VNAME="shoreside" USC_DATA_DIR="$MOOSIVP_USC_HOME/data"        \
-   SHARE_LISTEN="9300" VPORT="9000" SERVER_HOST=$SERVERHOST       \
+   SHARE_LISTEN=$SHORE_LISTEN VPORT=$SHORE_VPORT SERVER_HOST=$SERVERHOST       \
    LOCATION=$EXP_LOCATION  PLUG_DIR=$PLUGDIR  MSG_DIR=$MSGDIR     \
    PAINT_SEGLIST=$PAINTSEGLIST
 
@@ -57,16 +84,18 @@ MODEMID1="1"
 VTYPE1="UUV" # UUV, SHIP
 nsplug meta_vehicle.moos targ_$VNAME1.moos -f WARP=$TIME_WARP  \
    VNAME=$VNAME1  START_POS=$START_POS1  START_HDG=$START_HEADING \
-   VPORT="9001"       SHARE_LISTEN="9301" SERVER_LISTEN="9300" \
+   VPORT=$ANNA_VPORT SHARE_LISTEN=$ANNA_LISTEN SERVER_LISTEN=$SHORE_LISTEN \
    VTYPE=$VTYPE1      MODEMID=$MODEMID1                        \
    SERVER_HOST=$SERVERHOST  LOCATION=$EXP_LOCATION             \
    PLUG_DIR=$PLUGDIR  MSG_DIR=$MSGDIR                          \
-   LAWNMOWER=$LAWNMOWER_CONFIG
+   LAWNMOWER_CONFIG=$LAWNMOWER
 nsplug meta_vehicle.bhv targ_$VNAME1.bhv -f VNAME=$VNAME1      \
     START_POS=$START_POS1 WAYPOINTS=$WAYPOINTS1                \
     START_DEPTH=$START_DEPTH1 VTYPE=$VTYPE1                    \
     PILOT_LAWNMOWER_NS=$PILOT_LAWNMOWER_C_NS                   \
-    PILOT_LAWNMOWER_EW=$PILOT_LAWNMOWER_C_EW
+    PILOT_LAWNMOWER_EW=$PILOT_LAWNMOWER_C_EW                   \
+    LAWNMOWER_NS=$LAWNMOWERNS LAWNMOWER_EW=$LAWNMOWEREW        \
+    HP_LOITER=$HP_LOITER_CONFIG  ADAPTIVE_WPTS=$ADAPTIVE
 
 #VNAME2="ferdinand"      # The second vehicle community
 #START_POS2="2800,1900"
