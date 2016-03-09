a24d0c88a07929c6befd61a722b422b75b4c9db5
Stephanie Kemna <kemna@usc.edu>
2016-03-09 15:13:32 -0800

    start making parallel, add in param from launch script for filename predictions, add files for pAcommsHandler, add mode structure to bhv file, split up depth bhvs

--- missions/stephanie/test-bio-model/launch.sh
+++ missions/stephanie/test-bio-model/launch.sh
@@ -5,6 +5,7 @@
 TIME_WARP=1
 JUST_MAKE="no"
 ADAPTIVE="no"
+NUM_VEHICLES=1
 
 for ARGI; do
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
@@ -18,6 +19,8 @@ for ARGI; do
         JUST_MAKE="yes"
     elif [ "${ARGI}" = "--adaptive" -o "${ARGI}" = "-a" ] ; then
         ADAPTIVE="yes"
+    elif [ "${ARGI}" = "--2auvs" ] ; then
+        NUM_VEHICLES=2
     else 
         printf "Bad Argument: %s \n" $ARGI
         exit 0
@@ -39,19 +42,43 @@ PILOT_LAWNMOWER_CONFIG="format=lawnmower,label=pilot-survey,x=700,y=1100,width=4
 PILOT_LAWNMOWER_C_NS="$PILOT_LAWNMOWER_CONFIG,rows=north-south"
 PILOT_LAWNMOWER_C_EW="$PILOT_LAWNMOWER_CONFIG,rows=east-west"
 
+if [ $NUM_VEHICLES -ge 2 ] ; then
+PILOT_LM_1="format=lawnmower,label=pilot-survey,x=600,y=1100,width=200,height=200,lane_width=100,degs=0,startx=0,starty=0"
+PILOT_LM_1_NS="$PILOT_LM_1,rows=north-south"
+PILOT_LM_1_EW="$PILOT_LM_1,rows=east-west"
+PILOT_LM_2="format=lawnmower,label=pilot-survey,x=800,y=1100,width=200,height=200,lane_width=100,degs=0,startx=0,starty=0"
+PILOT_LM_2_NS="$PILOT_LM_2,rows=north-south"
+PILOT_LM_2_EW="$PILOT_LM_2,rows=east-west"LAWMMOWERNS2
+fi
+
 # loiter for during hyperparameter optimization
 HP_LOITER_CONFIG="format=radial,x=440,y=970,radius=10,pts=4,snap=1,label=hp_optimization_loiter"
+if [ $NUM_VEHICLES -ge 2 ] ; then
+HP_LOITER_CONFIG2="format=radial,x=470,y=990,radius=10,pts=4,snap=1,label=hp_optimization_loiter"
+fi
 
 # config for lawnmower for actual GP model building
-LAWNMOWER="format=lawnmower,label=east-west-survey,x=700,y=1100,width=400,height=200,lane_width=20,degs=0,startx=0,starty=0"
+LAWNMOWER="format=lawnmower,x=700,y=1100,width=400,height=200,lane_width=20,degs=0,startx=0,starty=0"
+if [ $NUM_VEHICLES -ge 2 ] ; then
+LAWNMOWER1="format=lawnmower,x=600,y=1100,width=200,height=200,lane_width=20,degs=0,startx=0,starty=0"
+LAWNMOWER2="format=lawnmower,x=800,y=1100,width=200,height=200,lane_width=20,degs=0,startx=0,starty=0"
+else
+LAWNMOWER1=$LAWNMOWER
+fi
 
 if [ "${ADAPTIVE}" = "no" ] ; then
   # lawnmower
-  LAWNMOWEREW="$LAWNMOWER,rows=east-west"
-  LAWNMOWERNS="$LAWNMOWER,rows=north-south"  
+  LAWNMOWEREW="$LAWNMOWER1,rows=east-west,label=east-west-survey"
+  LAWNMOWERNS="$LAWNMOWER1,rows=north-south,label=north-south-survey"
+  if [ $NUM_VEHICLES -ge 2 ] ; then
+  LAWNMOWEREW2="$LAWNMOWER2,rows=east-west,label=east-west-survey"
+  LAWNMOWERNS2="$LAWNMOWER2,rows=north-south,label=north-south-survey"
+  fi
   # ports
   ANNA_LISTEN="8301"
   ANNA_VPORT="8001"
+  FERDINAND_LISTEN="8302"
+  FERDINAND_VPORT="8002"
   SHORE_LISTEN="8300"
   SHORE_VPORT="8000"
 else
@@ -60,11 +87,11 @@ else
   # ports
   ANNA_LISTEN="9301"
   ANNA_VPORT="9001"
+  FERDINAND_LISTEN="9302"
+  FERDINAND_VPORT="9002"
   SHORE_LISTEN="9300"
   SHORE_VPORT="9000"
 fi
-
-
 
 SERVERHOST="localhost" #"localhost"
 nsplug meta_shoreside.moos targ_shoreside.moos -f WARP=$TIME_WARP \
@@ -78,17 +105,22 @@ START_HEADING="230"
 
 VNAME1="anna"        # The first  vehicle community
 START_DEPTH1="5"
-START_POS1="460,935"
+START_POS1="430,950"
 WAYPOINTS1="455,980:455,965:430,965:430,980:455,980"
 MODEMID1="1"
 VTYPE1="UUV" # UUV, SHIP
+PREDICTIONS_PREFIX1="anna_predictions"
 nsplug meta_vehicle.moos targ_$VNAME1.moos -f WARP=$TIME_WARP  \
    VNAME=$VNAME1  START_POS=$START_POS1  START_HDG=$START_HEADING \
    VPORT=$ANNA_VPORT SHARE_LISTEN=$ANNA_LISTEN SERVER_LISTEN=$SHORE_LISTEN \
    VTYPE=$VTYPE1      MODEMID=$MODEMID1                        \
    SERVER_HOST=$SERVERHOST  LOCATION=$EXP_LOCATION             \
    PLUG_DIR=$PLUGDIR  MSG_DIR=$MSGDIR                          \
-   LAWNMOWER_CONFIG=$LAWNMOWER
+   LAWNMOWER_CONFIG=$LAWNMOWER  PREDICTIONS_PREFIX=$PREDICTIONS_PREFIX1
+if [ $NUM_VEHICLES -ge 2 ] ; then
+PILOT_LAWNMOWER_C_NS=$PILOT_LM_1_NS
+PILOT_LAWNMOWER_C_EW=$PILOT_LM_1_EW
+fi
 nsplug meta_vehicle.bhv targ_$VNAME1.bhv -f VNAME=$VNAME1      \
     START_POS=$START_POS1 WAYPOINTS=$WAYPOINTS1                \
     START_DEPTH=$START_DEPTH1 VTYPE=$VTYPE1                    \
@@ -97,21 +129,30 @@ nsplug meta_vehicle.bhv targ_$VNAME1.bhv -f VNAME=$VNAME1      \
     LAWNMOWER_NS=$LAWNMOWERNS LAWNMOWER_EW=$LAWNMOWEREW        \
     HP_LOITER=$HP_LOITER_CONFIG  ADAPTIVE_WPTS=$ADAPTIVE
 
-#VNAME2="ferdinand"      # The second vehicle community
-#START_POS2="2800,1900"
-#WAYPOINTS2="2800,1900"
-#START_DEPTH2="10"
-#MODEMID2="6"
-#VTYPE2="UUV" # UUV, SHIP
-#nsplug meta_vehicle.moos targ_$VNAME2.moos -f WARP=$TIME_WARP  \
-#   VNAME=$VNAME2  START_POS=$START_POS2  START_HDG=$START_HEADING \
-#   VPORT="9002"       SHARE_LISTEN="9302"                      \
-#   VTYPE=$VTYPE2      MODEMID=$MODEMID2                        \
-#   SERVER_HOST=$SERVERHOST  LOCATION=$EXP_LOCATION
-#nsplug meta_vehicle.bhv targ_$VNAME2.bhv -f VNAME=$VNAME2      \
-#    START_POS=$START_POS2 WAYPOINTS=$WAYPOINTS2                \
-#    START_DEPTH=$START_DEPTH2 VTYPE=$VTYPE2
-
+if [ $NUM_VEHICLES -ge 2 ] ; then
+# The second vehicle community
+VNAME2="ferdinand"
+START_DEPTH2="5"
+START_POS2="450,950"
+WAYPOINTS2="455,980:455,965:430,965:430,980:455,980"
+MODEMID2="2"
+VTYPE2="UUV" # UUV, SHIP
+PREDICTIONS_PREFIX2="ferdinand_predictions"
+nsplug meta_vehicle.moos targ_$VNAME2.moos -f WARP=$TIME_WARP  \
+   VNAME=$VNAME2  START_POS=$START_POS2  START_HDG=$START_HEADING \
+   VPORT=$FERDINAND_VPORT SHARE_LISTEN=$FERDINAND_LISTEN SERVER_LISTEN=$SHORE_LISTEN \
+   VTYPE=$VTYPE2      MODEMID=$MODEMID2                        \
+   SERVER_HOST=$SERVERHOST  LOCATION=$EXP_LOCATION             \
+   PLUG_DIR=$PLUGDIR  MSG_DIR=$MSGDIR                          \
+   LAWNMOWER_CONFIG=$LAWNMOWER  PREDICTIONS_PREFIX=$PREDICTIONS_PREFIX2
+nsplug meta_vehicle.bhv targ_$VNAME2.bhv -f VNAME=$VNAME2      \
+    START_POS=$START_POS2 WAYPOINTS=$WAYPOINTS2                \
+    START_DEPTH=$START_DEPTH2 VTYPE=$VTYPE2                    \
+    PILOT_LAWNMOWER_NS=$PILOT_LM_2_NS                   \
+    PILOT_LAWNMOWER_EW=$PILOT_LM_2_EW                   \
+    LAWNMOWER_NS=$LAWNMOWERNS2 LAWNMOWER_EW=$LAWNMOWEREW2        \
+    HP_LOITER=$HP_LOITER_CONFIG2  ADAPTIVE_WPTS=$ADAPTIVE
+fi
 
 if [ ${JUST_MAKE} = "yes" ] ; then
     exit 0
@@ -125,17 +166,18 @@ pAntler targ_shoreside.moos > log_shoreside.log &
 
 printf "Launching $VNAME1 MOOS Community (WARP=%s) \n" $TIME_WARP
 pAntler targ_$VNAME1.moos > log_$VNAME1.log &
-#>& /dev/null &
 sleep .25
 
-#printf "Launching $VNAME2 MOOS Community (WARP=%s) \n" $TIME_WARP
-#pAntler targ_$VNAME2.moos > log_$VNAME2.log &
-#sleep .25
+if [ $NUM_VEHICLES -ge 2 ] ; then
+printf "Launching $VNAME2 MOOS Community (WARP=%s) \n" $TIME_WARP
+pAntler targ_$VNAME2.moos > log_$VNAME2.log &
+sleep .25
+fi
 
 printf "Done \n"
 
 uMAC targ_shoreside.moos
 
 printf "Killing all processes ... \n"
-kill %1 %2 #%3 %4
+kill %1 %2 %3 %4
 printf "Done killing processes.   \n"
