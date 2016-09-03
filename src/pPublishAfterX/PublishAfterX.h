49c865008109c605be7e880901e874f28bc34d38
Stephanie Kemna <kemna@usc.edu>
2016-09-03 15:01:24 -0700

    process to publish var/val after another specific var/val was published

---
+++ src/pPublishAfterX/PublishAfterX.h
@@ -0,0 +1,45 @@
+/*****************************************************************/
+/*    NAME: Stephanie Kemna                                      */
+/*    ORGN: Robotic Embedded Systems Lab, CS, USC, CA, USA       */
+/*    FILE: PublishAfterX.h                                      */
+/*    DATE: Sept 3, 2016                                         */
+/*                                                               */
+/*****************************************************************/
+
+#ifndef PPUBLISHAFTERX_HEADER
+#define PPUBLISHAFTERX_HEADER
+
+#include "MOOS/libMOOS/MOOSLib.h"
+
+class PublishAfterX : public CMOOSApp
+{
+  public:
+    PublishAfterX();
+    ~PublishAfterX() {};
+
+  protected:
+    // Standard MOOSApp functions to overload
+    bool OnNewMail(MOOSMSG_LIST &NewMail);
+    bool Iterate();
+    bool OnConnectToServer();
+    bool OnStartUp();
+
+    // Registration, Configuration, Mail handling utils
+    void registerVariables();
+
+  private:
+    // Own functions
+    void makeYourOwn();
+
+    // Configuration variables
+    std::string m_var;
+    std::string m_val;
+    size_t m_secs_after;
+    std::string m_pub_var;
+    std::string m_pub_val;
+
+    // State variables
+    double m_start_time;
+};
+
+#endif
