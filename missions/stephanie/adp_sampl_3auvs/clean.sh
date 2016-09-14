e0a3bc02ff3a6bfa2398d1928c4a5882d42d5fc0
Stephanie Kemna <kemna@usc.edu>
2016-09-13 20:55:59 -0700

    update clean script for new files

--- missions/stephanie/adp_sampl_3auvs/clean.sh
+++ missions/stephanie/adp_sampl_3auvs/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -r LOG_* *.log .LastOpenedMOOSLogDirectory .phostinfo targ_* *.*~ *predictions*.csv hp_optim*
+rm -r LOG_* *.log .LastOpenedMOOSLogDirectory .phostinfo targ_* *.*~ *predictions*.csv hp_optim* gp_*.txt vor_*.txt gp_*.txt node_*.txt
