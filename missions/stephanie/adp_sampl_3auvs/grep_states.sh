93e95ebead1d17724fad5f2e02bbacb8ba019a64
Stephanie <stephanie@daryl>
2016-10-28 17:57:41 -0700

    change script for fewer states, pGP without window as default

--- missions/stephanie/adp_sampl_3auvs/grep_states.sh
+++ missions/stephanie/adp_sampl_3auvs/grep_states.sh
@@ -10,8 +10,6 @@ grep_all ()
 
   grep " STAGE " ${1}*/*.alog > stage_${2}.txt
   grep " STATE_MISSION " ${1}*/*.alog > stateM_${2}.txt
-  grep " STATE_SURFACE " ${1}*/*.alog > stateS_${2}.txt
-  grep " STATE_DATA_SHARING " ${1}*/*.alog > stateD_${2}.txt
 }
 
 grep_all LOG_A a
