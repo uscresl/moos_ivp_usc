a7b3e8cd311bfcce5c4e869a68ea7eb4977dda3d
Stephanie <stephanie@daryl>
2017-05-23 10:49:13 -0700

    cleaning up

--- missions/stephanie/adp_sampl_3auvs/clean.sh
+++ missions/stephanie/adp_sampl_3auvs/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -r LOG_* *.log .LastOpenedMOOSLogDirectory .phostinfo targ_* *.*~ *predictions*.csv hp_optim* gp_*.txt vor_*.txt gp_*.txt node_*.txt
+rm -r LOG_* *.log .LastOpenedMOOSLogDirectory .phostinfo targ_* *.*~ *predictions*.csv hp_optim* gp_*.txt vor_*.txt gp_*.txt node_*.txt output.txt
