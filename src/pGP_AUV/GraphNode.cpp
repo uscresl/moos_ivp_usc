7a70ad2cf117ceb231aa3b64a8c8bd33731fd8de
Stephanie Kemna <kemna@usc.edu>
2017-06-23 18:53:16 -0700

    cosmetics

--- src/pGP_AUV/GraphNode.cpp
+++ src/pGP_AUV/GraphNode.cpp
@@ -1,12 +1,12 @@
 #include "GraphNode.h"
 
-GraphNode::GraphNode(Eigen::Vector2d location, double value)
+GraphNode::GraphNode(Eigen::Vector2d location, double value) :
+  m_location(location),
+  m_value(value)
 {
-  m_location = location;
-  m_value = value;
   m_neighbours.reserve(MAX_NEIGHBOURS);
   for( int i = 0; i < MAX_NEIGHBOURS; i++ ){
-    m_neighbours.push_back(NULL); 
+    m_neighbours.push_back(NULL);
   }
 }
 
@@ -20,11 +20,12 @@ GraphNode::GraphNode(Eigen::Vector2d location, double value, GraphNode *left_nei
 
 // The first four elements in vector are assigned as neighbours in the order: left, right, front and back.
 // If less than four elements are present then NULL is assigned to remaining neighbours.
-GraphNode::GraphNode(Eigen::Vector2d location, double value, std::vector<GraphNode*> neighbours):GraphNode(location,value)
+GraphNode::GraphNode(Eigen::Vector2d location, double value, std::vector<GraphNode*> neighbours) :
+  GraphNode(location,value)
 {
   for( int i = 0; i < neighbours.size() && i<MAX_NEIGHBOURS ; i++ ){
     m_neighbours[i] = neighbours[i];
-  } 
+  }
 }
 
 // All neighbours returned in order: left, right, front and back.
@@ -34,7 +35,7 @@ std::vector<GraphNode*> GraphNode::get_neighbours() const
 }
 
 // Neighbours excluding back returned in order: left, right and front.
-std::vector<GraphNode*> GraphNode::get_neighbours_ahead() const 
+std::vector<GraphNode*> GraphNode::get_neighbours_ahead() const
 {
   return std::vector<GraphNode*>(m_neighbours.begin(), m_neighbours.begin() + MAX_NEIGHBOURS_AHEAD);
 }
@@ -44,7 +45,7 @@ const GraphNode * GraphNode::get_left_neighbour() const
   return m_neighbours[LEFT_NEIGHBOUR];
 }
 
-void GraphNode::set_left_neighbour(GraphNode * left_neighbour) 
+void GraphNode::set_left_neighbour(GraphNode * left_neighbour)
 {
   m_neighbours[LEFT_NEIGHBOUR] = left_neighbour;
 }
@@ -91,7 +92,7 @@ void GraphNode::set_value(double value)
 
 void GraphNode::set_location(Eigen::Vector2d location)
 {
-  m_location = location; 
+  m_location = location;
 }
 
 Eigen::Vector2d GraphNode::get_location() const
--- src/pGP_AUV/GraphNode.h
+++ src/pGP_AUV/GraphNode.h
@@ -5,59 +5,55 @@
 
 class GraphNode
 {
-public:
-  GraphNode(Eigen::Vector2d location, double value);
+  public:
+    // constructors //////////////////////////////////////////////////////////////
+    GraphNode(Eigen::Vector2d location, double value);
 
-  GraphNode(Eigen::Vector2d location, double value, GraphNode *left_neighbour, GraphNode *right_neighbour, GraphNode *front_neighbour, GraphNode *back_neighbour);
+    GraphNode(Eigen::Vector2d location, double value, GraphNode *left_neighbour, GraphNode *right_neighbour, GraphNode *front_neighbour, GraphNode *back_neighbour);
 
-  // Neighbours expected to be in the order: left, right, front and back.
-  GraphNode(Eigen::Vector2d location, double value, std::vector<GraphNode*> neighbours);
+    // Neighbours expected to be in the order: left, right, front and back.
+    GraphNode(Eigen::Vector2d location, double value, std::vector<GraphNode*> neighbours);
 
-  // All neighbours are returned in order: left, right, front and back.
-  std::vector<GraphNode*> get_neighbours() const;
 
-  // Returns neighbours excluding back in order: left, right and front.
-  std::vector<GraphNode*> get_neighbours_ahead() const;
+    // All neighbours are returned in order: left, right, front and back.
+    std::vector<GraphNode*> get_neighbours() const;
 
-  const GraphNode* get_left_neighbour() const;
+    // Returns neighbours excluding back in order: left, right and front.
+    std::vector<GraphNode*> get_neighbours_ahead() const;
 
-  void set_left_neighbour(GraphNode* left_neighbour);
+    const GraphNode* get_left_neighbour() const;
+    void set_left_neighbour(GraphNode* left_neighbour);
 
-  const GraphNode* get_right_neighbour() const;
+    const GraphNode* get_right_neighbour() const;
+    void set_right_neighbour(GraphNode* right_neighbour);
 
-  void set_right_neighbour(GraphNode* right_neighbour);
+    const GraphNode* get_front_neighbour() const;
+    void set_front_neighbour(GraphNode* front_neighbour);
 
-  const GraphNode* get_front_neighbour() const;
+    const GraphNode* get_back_neighbour() const;
+    void set_back_neighbour(GraphNode* back_neighbour);
 
-  void set_front_neighbour(GraphNode* front_neighbour);
+    double get_value() const;
+    void set_value(double value);
 
-  const GraphNode* get_back_neighbour() const;
+    void set_location(Eigen::Vector2d location);
+    Eigen::Vector2d get_location() const;
 
-  void set_back_neighbour(GraphNode* back_neighbour);
+    // Represent indices of the neighbours in the neighbours vector.
+    static const int LEFT_NEIGHBOUR=0, RIGHT_NEIGHBOUR=1, FRONT_NEIGHBOUR=2, BACK_NEIGHBOUR=3;
 
-  double get_value() const;
+  private:
+    // Holds neighbours of the node in the order left, right, front and back.
+    std::vector<GraphNode*> m_neighbours;
 
-  void set_value(double value);
+    // Entropy
+    double m_value;
 
-  void set_location(Eigen::Vector2d location);
+    // Latitude and longitude
+    Eigen::Vector2d m_location;
 
-  Eigen::Vector2d get_location() const;
-
-  // Represent indexes of the neighbours in the neighbours vector.
-  static const int LEFT_NEIGHBOUR=0, RIGHT_NEIGHBOUR=1, FRONT_NEIGHBOUR=2, BACK_NEIGHBOUR=3;
-
-private:
-  // Holds neighbours of the node in the order left, right, front and back.
-  std::vector<GraphNode*> m_neighbours;
-
-  // Entropy
-  double m_value;
-
-  // Latitude and longitude
-  Eigen::Vector2d m_location;
-
-  static const int MAX_NEIGHBOURS = 4;
-  static const int MAX_NEIGHBOURS_AHEAD = 3;
+    static const int MAX_NEIGHBOURS = 4;
+    static const int MAX_NEIGHBOURS_AHEAD = 3;
 };
 
 #endif
