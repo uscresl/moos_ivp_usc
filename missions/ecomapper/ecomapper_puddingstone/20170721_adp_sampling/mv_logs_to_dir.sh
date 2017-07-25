a38af12609dcf25f5f5adad08bdf2e4a368675d3
luna <uscresl@gmail.com>
2017-07-24 18:09:31 -0700

    update mv_logs script

--- missions/ecomapper/ecomapper_puddingstone/20170721_adp_sampling/mv_logs_to_dir.sh
+++ missions/ecomapper/ecomapper_puddingstone/20170721_adp_sampling/mv_logs_to_dir.sh
@@ -8,4 +8,4 @@ fi
 folder=$1
 
 mkdir $1
-mv MOOSLog* log* predictions*.csv  hp_optim* *.log *.moos ecomapper.bhv LOG_ZOOMER* output.txt $folder
+mv MOOSLog* LOG_* log* predictions*.csv  hp_optim* *.log *.moos ecomapper.bhv LOG_ZOOMER* output.txt $folder
