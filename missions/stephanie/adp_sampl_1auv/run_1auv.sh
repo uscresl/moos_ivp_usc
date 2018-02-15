b387fc1411281ead82c8070a6ce05ee9708aa8a7
Stephanie <kemna@usc.edu>
2018-02-14 17:39:57 -0800

    remove tee, problems cause of mac?

--- missions/stephanie/adp_sampl_1auv/run_1auv.sh
+++ missions/stephanie/adp_sampl_1auv/run_1auv.sh
@@ -11,7 +11,7 @@ folder=$1
 # 1 AUV dyn prog run
 for idx in `seq 1 30`; do
 	# start sim
-	./launch.sh 3 |&tee output.txt &
+	./launch.sh 3
 	sleep 30 && uPokeDB targ_shoreside.moos "DEPLOY_ALL=true"
 
 	# wait for sim to run, then end it
